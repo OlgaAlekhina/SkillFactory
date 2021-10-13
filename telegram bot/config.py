110f75bdb820fb6ae5db3f1a8cae3d9e4b24370d
OlgaAlekhina <89858498+OlgaAlekhina@users.noreply.github.com>
2021-10-13 11:47:42 +0300

    Update config.py

--- telegram bot/config.py
+++ telegram bot/config.py
@@ -1,6 +1,6 @@
 # API fastForex.io 214eba4cb0-5df28e954c-r0m3g8    ключ действует до 14 октября
 
-TOKEN = '2032018538:AAHeeEzD4UjMVQ7ws3CnumXPQfJ2swTPIig'
+TOKEN = 
 my_api_key = '214eba4cb0-5df28e954c-r0m3g8'
 
 keys = {
